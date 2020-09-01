4e7fbe0a3e60b810b94de168478caf1518b53e27
Deepshikha Gupta <deep.gupta.6391@gmail.com>
2020-09-01 16:32:34 -0400

    locators added in deals page

--- src/main/java/com/qa/hubspot/page/DealsPage.java
+++ src/main/java/com/qa/hubspot/page/DealsPage.java
@@ -1,10 +1,16 @@
 package com.qa.hubspot.page;
 
+import org.openqa.selenium.By;
+
 import com.qa.hubspot.base.BasePage;
 
 public class DealsPage extends BasePage{
+	
+	By dealsHeader=By.xpath("//span[text()='Deals']");
 
 	public DealsPage() {
 		System.out.println("Deals page const..........");
 	}
+	
+	
 }
